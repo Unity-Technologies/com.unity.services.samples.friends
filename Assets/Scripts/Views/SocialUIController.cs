2f0b21e15113010145fccd406bfe2e33b397dec2
UnityJacob <jacob.lorentzen@unity3d.com>
2022-09-23 10:05:53 -0700

    fix: runtime theme
    Makes the UI closer to what you actually get at runtime.

---
+++ Assets/Scripts/Views/SocialUIController.cs
@@ -0,0 +1,19 @@
+using System;
+
+using UnityEngine;
+using UnityEngine.UIElements;
+
+[RequireComponent(typeof(UIDocument))]
+public class SocialUIController : MonoBehaviour
+{
+    [SerializeField]
+    UIDocument m_SocialUIDoc;
+
+
+
+
+    void Start()
+    {
+
+    }
+}
