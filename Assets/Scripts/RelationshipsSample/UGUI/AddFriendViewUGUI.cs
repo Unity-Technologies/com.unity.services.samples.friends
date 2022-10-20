d64dae641b0b52fb793b84a26fd4a3423ea468f0
Pierre Dalaya <dalaya.arjun@gmail.com>
2022-10-20 14:40:31 -0400

    add request result text

--- Assets/Scripts/RelationshipsSample/UGUI/AddFriendViewUGUI.cs
+++ Assets/Scripts/RelationshipsSample/UGUI/AddFriendViewUGUI.cs
@@ -11,12 +11,17 @@ namespace UnityGamingServicesUsesCases.Relationships.UGUI
         [SerializeField] private Button m_CloseButton = null;
         [SerializeField] private Button m_BackgroundButton = null;
         [SerializeField] private TMP_InputField m_IdInputField = null;
+        [SerializeField] private TextMeshProUGUI m_RequestResultText = null;
 
         public void Init()
         {
             var playerId = string.Empty;
             m_IdInputField.onValueChanged.AddListener((value) => { playerId = value; });
-            m_AddFriendButton.onClick.AddListener(() => onFriendRequestSent?.Invoke(playerId));
+            m_AddFriendButton.onClick.AddListener(() =>
+            {
+                m_RequestResultText.text = string.Empty;
+                onFriendRequestSent?.Invoke(playerId);
+            });
             m_BackgroundButton.onClick.AddListener(Hide);
             m_CloseButton.onClick.AddListener(Hide);
             Hide();
@@ -24,18 +29,20 @@ namespace UnityGamingServicesUsesCases.Relationships.UGUI
         
         public void FriendRequestSuccess()
         {
-            
+            m_RequestResultText.text = "Friend request sent!";
         }
 
         public void FriendRequestFailed()
         {
-          //
+            m_RequestResultText.text = "could not send request : no player with that ID";
         }
 
         public Action<string> onFriendRequestSent { get; set; }
 
         public void Show()
         {
+            m_RequestResultText.text = string.Empty;
+            m_IdInputField.SetTextWithoutNotify("");
             gameObject.SetActive(true);
         }
 
