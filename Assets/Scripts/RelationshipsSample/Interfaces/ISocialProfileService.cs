459d688d5bc8f06e40ec439e6b8facb44aeaf6f0
Jacob <jacob.lorentzen@unity3D.com>
2022-10-19 17:05:26 -0700

    fix:Added some comments for the interface

--- Assets/Scripts/RelationshipsSample/Interfaces/ISocialProfileService.cs
+++ Assets/Scripts/RelationshipsSample/Interfaces/ISocialProfileService.cs
@@ -1,5 +1,9 @@
 namespace UnityGamingServicesUsesCases.Relationships
 {
+    /// <summary>
+    /// Extend this interface and add to it for your own user profile system, like adding support for user Icons,
+    /// and other information you want to show in the Relationship UI
+    /// </summary>
     public interface ISocialProfileService
     {
         string GetName(string playerID);
