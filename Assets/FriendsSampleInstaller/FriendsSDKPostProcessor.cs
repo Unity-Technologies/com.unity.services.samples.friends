75925bd2d7bb6d0869cb2187364e658b3d59f1dd
Pierre Dalaya <pierre.dalaya@unity3d.com>
2023-04-05 13:46:33 -0400

    friends sample installer

--- Assets/FriendsSampleInstaller/FriendsSDKPostProcessor.cs
+++ Assets/FriendsSampleInstaller/FriendsSDKPostProcessor.cs
@@ -1,21 +1,16 @@
 using UnityEditor;
-using UnityEngine;
 
 public class FriendsSDKPostProcessor : AssetPostprocessor
 {
     private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets,
         string[] movedFromAssetPaths)
     {
-      
-        foreach (var imported in importedAssets)
+        foreach (var importedAsset in importedAssets)
         {
-            Debug.Log(imported);
-            if (imported == "Packages/com.unity.services.friends/Runtime/SDK/FriendsApi.cs")
-            {
-                Debug.Log("Detected friends package import");
-                var path = "Assets/FriendsSampleInstaller/Package/FriendsSample.unitypackage";
-                AssetDatabase.ImportPackage(path, true);
-            }
+            if (importedAsset != "Packages/com.unity.services.friends") 
+                continue;
+            
+            FriendsSampleInstaller.InstallFriendsSampleUnityPackage();
         }
     }
 }
--- Assets/FriendsSampleInstaller/FriendsSampleInstaller.cs
+++ Assets/FriendsSampleInstaller/FriendsSampleInstaller.cs
@@ -1,18 +1,37 @@
+using System.Linq;
 using System.Threading.Tasks;
 using UnityEditor;
 using UnityEditor.PackageManager;
+using UnityEditor.PackageManager.Requests;
 
 public static class FriendsSampleInstaller
 {
     [MenuItem("Tools/UGSSamples/FriendsSample/Install")]
     public static async void Install()
     {
-        var addAndRemoveRequest = Client.AddAndRemove(new[]
-        {
-            "com.unity.services.friends@0.2.0-preview.9"
-        });
+        var packages = Client.List();
 
-        while (!addAndRemoveRequest.IsCompleted)
+        while (!packages.IsCompleted)
             await Task.Delay(100);
+
+        if (IsPackageInstalled(packages, "com.unity.services.friends"))
+        {
+            //Then we can install the friends sample package safely
+            InstallFriendsSampleUnityPackage();
+            return;
+        }
+
+        Client.AddAndRemove(new[] { "com.unity.services.friends@0.2.0-preview.9" });
+    }
+
+    private static bool IsPackageInstalled(ListRequest packages, string packageName)
+    {
+        return packages.Result.Any(package => package.name.Equals(packageName));
+    }
+
+    public static void InstallFriendsSampleUnityPackage()
+    {
+        var friendsSamplePackagePath = "Assets/FriendsSampleInstaller/Package/FriendsSample.unitypackage";
+        AssetDatabase.ImportPackage(friendsSamplePackagePath, true);
     }
 }
